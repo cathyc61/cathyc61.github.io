e14bc898f3a01b937155f8de543cbd60ada07267
cathyc61 <31240535+cathyc61@users.noreply.github.com>
2026-03-07 16:04:58 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Front Page
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/editor/cathyc61/pen/019cc6d9-fdde-722a-9fb3-64fb60a81de7](https://codepen.io/editor/cathyc61/pen/019cc6d9-fdde-722a-9fb3-64fb60a81de7).
+
---
+++ script.js
@@ -0,0 +1,16 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    
+    // Toggle icon between bars (hamburger) and times (X)
+    const icon = mobileMenu.querySelector('i');
+    if(navLinks.classList.contains('active')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-xmark');
+    } else {
+        icon.classList.remove('fa-xmark');
+        icon.classList.add('fa-bars');
+    }
+});
